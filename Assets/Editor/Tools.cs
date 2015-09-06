10eb4076df026519179ceee84333178a32f35ab4
bigrpg <bigrpg@163.com>
2015-09-06 22:36:09 +0800

    modify

--- Assets/Editor/Tools.cs
+++ Assets/Editor/Tools.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
 
 public class CreateAssetBundles
 {
@@ -6,5 +8,35 @@ public class CreateAssetBundles
     static void BuildAllAssetBundles()
     {
         BuildPipeline.BuildAssetBundles("AssetBundles", BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+
+
+    [MenuItem("Assets/Auto Set AssetBundleName")]
+    static void AutoSetAssetBundleName()
+    {
+        Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        List<string> alist = new List<string>();
+        foreach (Object one in selection)
+        {
+            string path = AssetDatabase.GetAssetPath(one);
+            alist.Add(path);
+        }
+
+        string[] abPaths = alist.ToArray() as string[];
+        string[] dps = AssetDatabase.GetDependencies(abPaths);
+        for (int i = 0; i < dps.Length; ++i)
+        {
+            if (alist.IndexOf(dps[i]) >= 0)
+                continue;
+
+            AssetImporter ai = AssetImporter.GetAtPath(dps[i]);
+            if (ai.assetBundleName != null)
+            {
+                string abName = AssetDatabase.AssetPathToGUID(dps[i]);
+                UnityEngine.Debug.Log(string.Format("No.{0} obj's id is:{1}", i + 1, abName));
+                ai.assetBundleName = abName;
+            }
+        }
     }
 }
