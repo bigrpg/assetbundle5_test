800243ac3cd44eef7b02129ad363d00601cbec17
bigrpg <bigrpg@163.com>
2015-09-20 20:46:14 +0800

    modify

---
+++ Assets/Scripts/FollowAvatar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowAvatar : MonoBehaviour {
+
+    public  GameObject avatar;
+    Animator ani;
+
+	// Use this for initialization
+	void Start () {
+        ani = avatar.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        transform.position = ani.bodyPosition + new Vector3(2, 1, 2);
+	
+	}
+}
---
+++ Assets/Scripts/PalyAni.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PalyAni : MonoBehaviour {
+
+    Animator ani;
+	// Use this for initialization
+	void Start () {
+
+        ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+            ani.SetInteger("weapon", 1);
+        else if (Input.GetKey(KeyCode.B))
+            ani.SetInteger("weapon", 0);
+
+        if (Input.GetKey(KeyCode.Alpha0))
+            ani.SetFloat("speed", 0f);
+        else if (Input.GetKey(KeyCode.Alpha1))
+            ani.SetFloat("speed", 5f);
+        else if (Input.GetKey(KeyCode.Alpha2))
+            ani.SetFloat("speed", 8f);
+	
+	}
+}
